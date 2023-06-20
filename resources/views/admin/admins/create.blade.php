8651db5c4c5fb901bbfcb96b41a2f15a7d1aec52
mahmoudmohamed <mahmoudmohamed199885.com>
2023-06-21 02:53:43 +0300

    f'

--- resources/views/admin/admins/create.blade.php
+++ resources/views/admin/admins/create.blade.php
@@ -32,8 +32,8 @@ Add admin
 <div class="breadcrumb-header justify-content-between">
     <div class="my-auto">
         <div class="d-flex">
-            <h4 class="content-title mb-0 my-auto">admins</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0"> /
-                admins</span>
+            <h4 class="content-title mb-0 my-auto">@lang('lang.admins')</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0"> /
+                @lang('lang.admins')</span>
         </div>
     </div>
 </div>
